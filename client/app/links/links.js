298c38c72cbdcda7552db31adb4e0d0cb0484fcf
jindac <dachanman91@gmail.com>
2017-01-21 12:17:54 -0800

    Add route, template, and controller for /links and /shorten

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,8 +1,8 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
   $scope.data = {};
-  Links.getAll();
-  // console.log(Links.getAll());
+  Links.getAll().then(function(allLinks) {
+    $scope.data.links = allLinks;
+  });
 });
